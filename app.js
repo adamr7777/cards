ad0ba08db1d9277c8e7965ec1e330ab7ffd99aea
adomasr <adomas.razmus@gmail.com>
2022-12-23 19:52:55 +0000

    2

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ const shuffleBtn = document.getElementById('shuffle-btn');
 
 
 
-shuffleBtn.addEventListener('click', function() {
+shuffleBtn.addEventListener('click', shuffleCards);
+
+
+
+
+
+function shuffleCards() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then((response) => response.json())
         .then((data) => console.log(data));
-})
+}
+
+
+setTimeout(runF, 2000);
+
+
+function runF() {
+    console.log('finnaly I ran')
+}
