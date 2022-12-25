9b6d4c5558653862b3d0b3c3937880c7ffad56ee
adomasr <adomas.razmus@gmail.com>
2022-12-25 05:29:56 +0000

    deal2cards

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
+//https://apis.scrimba.com/deckofcards/api/deck/<<deck_id>>/draw/?count=2
 
-//https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
+
+//variables
 
 const shuffleBtn = document.getElementById('shuffle-btn');
+const deal2  = document.getElementById('deal2');
+const cardsContainer = document.getElementById('cards-container');
 
 
 
+
+//Eventlisteners
 
 
 shuffleBtn.addEventListener('click', shuffleCards);
 
 
+deal2.addEventListener('click', deal2Cards);
 
+
+
+//Functions
 
 
 function shuffleCards() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            const deckId = data.deck_id
+            localStorage.setItem('deckId', JSON.stringify(deckId))
+            console.log('shuffled! your deck id is ' + deckId);
+        });
 }
 
 
-setTimeout(runF, 2000);
 
-
-function runF() {
-    console.log('finnaly I ran')
+function deal2Cards() {
+    const deckId = JSON.parse(localStorage.getItem('deckId'))
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then (res => res.json())
+        .then (data => {
+            console.log(data);
+            let htmlString = '';
+            for (let element of data.cards) {
+                htmlString += `<img src='${element.image}'/>`
+            }
+            cardsContainer.innerHTML = htmlString;
+        })
+    
 }
+
